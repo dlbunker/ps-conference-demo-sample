c2051b17d4597252169a975f7ee3b9fdfeb15803
dlbunker <mcirque@gmail.com>
2019-10-22 22:44:37 -0600

    Update PersistenceConfiguration.java

--- src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -1,14 +1,11 @@
 package com.pluralsight.conferencedemo.config;
 
-import javax.sql.DataSource;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
 public class PersistenceConfiguration {
-    @Bean
+    // @Bean
     // public DataSource dataSource() {
     //     DataSourceBuilder builder = DataSourceBuilder.create();
     //     builder.url("jdbc:postgresql://localhost:5432/conference_app");
